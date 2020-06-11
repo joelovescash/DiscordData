e28aec97d25d172452b4eefed1ff057bb40d5377
joelovescash <41713109+joelovescash@users.noreply.github.com>
2020-06-11 11:47:18 -0400

    Raw Data Dump

--- discord data.py
+++ discord data.py
@@ -1,1 +1,2 @@
-print("hello world)")
+textfile = open(r"C:\Users\cland\Documents\GitHub\DiscordData\rotow - Text - pit-of-unfortune [490245096491581450] thru 06112020.txt", encoding="utf8")
+textfile = textfile.read()
